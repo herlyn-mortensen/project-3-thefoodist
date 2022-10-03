dc33ebf82a8db68ede87b496bcb16e8426401e2a
Herlyn Mortensen <herlynmortensen@gmail.com>
2022-10-03 12:06:34 +0000

    started vue

---
+++ src/components/Basic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="title">Vue welcomes you</h1>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'Basic',
+}
+</script>
+ 
+<style>
+.title {
+  font-family: Verdana;
+}
+</style>
