40a12fa5560897247179b34688188837c96be4b1
Herlyn Mortensen <herlynmortensen@gmail.com>
2022-10-10 11:24:10 +0000

    added components

---
+++ src/components/atom/button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button type="button" class="btn btn-dark">Dark</button>
+</template>
+ 
+<script>
+export default {
+  name: 'Basic',
+}
+</script>
+ 
+<style>
+.title {
+  font-family: Verdana;
+}
+</style>
---
+++ src/components/atom/textField.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="title">Form</h1>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'Basic',
+}
+</script>
+ 
+<style>
+.title {
+  font-family: Verdana;
+}
+</style>
