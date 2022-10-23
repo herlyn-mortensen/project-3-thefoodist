83a9aa7df9800e911e0e458ddabfc27f16cf4884
Herlyn Mortensen <herlynmortensen@gmail.com>
2022-10-23 16:46:32 +0000

    -fixed the button arrangement.

--- src/components/FindReview.vue
+++ src/components/FindReview.vue
@@ -7,19 +7,21 @@
         <input type="text" class="form-control-find" name="restaurant" />
       </form>
 
-      <div class="d-flex buttons-find">
-        <div class="p-2">
-          <input type="submit" class="btn btn-dark mt-3" value="Search" />
-        </div>
-        <div class="p-2">
-          <input type="submit" class="btn btn-dark mt-3" value="Delete" />
-        </div>
-        <div class="p-2">
-          <input
-            type="submit"
-            class="btn btn-light border-dark mt-3"
-            value="Edit"
-          />
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <input type="submit" class="btn btn-dark mt-3 submitButton" value="Search" />
+          </div>
+          <div class="col-sm">
+            <input type="submit" class="btn btn-dark mt-3 submitButton" value="Delete" />
+          </div>
+          <div class="col-sm">
+            <input
+              type="submit"
+              class="btn btn-light border-dark mt-3 submitButton"
+              value="Edit"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -37,31 +39,30 @@ export default {
   height: 100vh;
 }
 
-
-.form-control-find{
-  width: 100%;  
-  height:48px;
-  border-radius:8px;
-  box-shadow: 1px;  
+.form-control-find {
+  width: 100%;
+  height: 48px;
+  border-radius: 8px;
+  box-shadow: 1px;
   font-size: 1.25rem;
   border-radius: 5px;
   border: 1px solid #555;
   border-color: rgb(226, 226, 226);
   padding: 12px 20px;
-  display: inline
+  display: inline;
 }
 
-.label-find {  
+.label-find {
   text-align: left;
   width: 100%;
-  
 }
 
-.buttons-find {
-  margin-left: 20px;
+.submitButton{
+  width: 100%;
+  height:48px;
 }
 
-.boundary{
+.boundary {
   max-width: 900px;
   margin-left: auto;
   margin-right: auto;
